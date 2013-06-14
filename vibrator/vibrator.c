ba090ac6f48a28660e3e688d9db1b7ddcf21bba9
Pablo Lunarenko <kimiaropablo958@gmail.com>
2013-06-14 14:06:08 -0300

    Commit for PAC thread

--- vibrator/vibrator.c
+++ vibrator/vibrator.c
@@ -21,24 +21,35 @@
 #include <fcntl.h>
 #include <errno.h>
 
-
 #define THE_DEVICE "/sys/class/timed_output/vibrator/enable"
 
 int vibrator_exists()
 {
     int fd;
 
-    fd = open(THE_DEVICE, O_RDWR);
-    if(fd != 0)
+#ifdef QEMU_HARDWARE
+    if (qemu_check()) {
         return 1;
+    }
+#endif
+
+    fd = open(THE_DEVICE, O_RDWR);
+    if(fd < 0)
+        return 0;
     close(fd);
-    return 0;
+    return 1;
 }
 
 static int sendit(int timeout_ms)
 {
     int nwr, ret, fd;
     char value[20];
+
+#ifdef QEMU_HARDWARE
+    if (qemu_check()) {
+        return qemu_control_command( "vibrator:%d", timeout_ms );
+    }
+#endif
 
     fd = open(THE_DEVICE, O_RDWR);
     if(fd < 0)
@@ -52,17 +63,15 @@ static int sendit(int timeout_ms)
     return (ret == nwr) ? 0 : -1;
 }
 
-
 int vibrator_on(int timeout_ms)
 {
     /* constant on, up to maximum allowed time */
-	if(timeout_ms < 0)
-		return sendit(5000);
-	return sendit(timeout_ms);
+    if(timeout_ms < 0)
+    return sendit(5000);
+    return sendit(timeout_ms);
 }
 
 int vibrator_off()
-{	
-	/* checks if the device is vibrating, if it's vibrating stop, if not, do nothing */
-	return (vibrator_exists() == 0) ? 0 : 1;
+{
+    return sendit(0);
 }
